d90f7779f0a8d60463c7cadb8a274cdeb9ff21a2
Benjamin Gaal <benjamin.gaal@tugraz.at>
2021-04-21 13:14:50 +0200

    EXPDAT-003 database save entry [BG, MZ]

---
+++ app/src/androidTest/java/at/sw21_tug/team_25/expirydates/DatabaseTests.kt
@@ -0,0 +1,95 @@
+package at.sw21_tug.team_25.expirydates
+
+import androidx.annotation.MainThread
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.Observer
+import androidx.test.platform.app.InstrumentationRegistry
+import at.sw21_tug.team_25.expirydates.data.ExpItem
+import at.sw21_tug.team_25.expirydates.data.ExpItemDao
+import at.sw21_tug.team_25.expirydates.data.ExpItemDatabase
+import at.sw21_tug.team_25.expirydates.data.ExpItemRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import org.junit.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.IOException
+
+
+/**
+ * Example database tests, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class DatabaseTests {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var expItemDao: ExpItemDao
+    private lateinit var db: ExpItemDatabase
+    private lateinit var repository: ExpItemRepository
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = ExpItemDatabase.getDatabase(context)
+                //Room.databaseBuilder(context, ExpItemDatabase::class.java, "expitem-database").build()
+        expItemDao = db.expItemDao()
+        repository = ExpItemRepository(expItemDao)
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    /*
+    Test to create a database Item and retrieve it.
+     */
+    @Test @MainThread
+    @Throws(Exception::class)
+    fun createTest() {
+        // Delete all items in the database
+        expItemDao.deleteAllItems()
+
+        // Create fake data
+        val name = "Salami"
+        val date = "22.04.2021"
+
+        // Create Item Object
+        val item = ExpItem(name, date)
+
+        // Add Item to database
+        GlobalScope.async {
+            expItemDao.insertItem(item)
+        }
+
+        // Get items from database
+        val items = expItemDao.readAllItems()
+
+        val lifecycleOwner: LifecycleOwner = mockLifecycleOwner()  // Mockito.mock(LifecycleOwner::class.java)
+        items.observe(lifecycleOwner, Observer<List<ExpItem>> {
+            if (it != null) {
+                Assert.assertTrue(
+                        "The amount of items in the database is incorrect: "
+                                + it.size.toString(),
+                        1 == it.size)
+            }
+        })
+    }
+
+    private fun mockLifecycleOwner(): LifecycleOwner {
+        val owner: LifecycleOwner = Mockito.mock(LifecycleOwner::class.java)
+        val lifecycle = LifecycleRegistry(owner)
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        `when`(owner.lifecycle).thenReturn(lifecycle)
+        return owner
+    }
+}
