330cc696b3be6b89b54a6f36cdbbd1f4e7214cfd
zojems08 <matthias.zojer@student.tugraz.at>
2021-04-23 15:37:57 +0200

    EXPDAT-018 Inputed data is now saved in database. [WB,ZM]

--- app/src/main/java/at/sw21_tug/team_25/expirydates/misc/Util.kt
+++ app/src/main/java/at/sw21_tug/team_25/expirydates/misc/Util.kt
@@ -6,7 +6,7 @@ import java.util.*
 class Util {
     companion object {
         fun convertDateToString(long: Long) : String {
-            val formatter = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY)
+            val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.GERMANY)
             return formatter.format(long)
         }
     }
