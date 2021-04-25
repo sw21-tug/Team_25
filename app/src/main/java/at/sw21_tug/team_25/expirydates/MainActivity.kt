14e249d6b39b39d81aada449682f957a6fe9c2da
Christopher Fartek <fartek@student.tugraz.at>
2021-04-25 18:41:04 +0200

    EXPDAT-008 added basic Detail view, DetailViewActivity.kt [FC, MF]

--- app/src/main/java/at/sw21_tug/team_25/expirydates/MainActivity.kt
+++ app/src/main/java/at/sw21_tug/team_25/expirydates/MainActivity.kt
@@ -1,12 +1,16 @@
 package at.sw21_tug.team_25.expirydates
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import at.sw21_tug.team_25.expirydates.ui.detailview.ui.DetailViewActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +26,20 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_add, R.id.navigation_list))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun openDetailView(view: View) {
+
+        val bundle = Bundle();
+        val names = arrayOf("Milk", "Eggs", "Bread");
+
+        val time = System.currentTimeMillis()
+
+        bundle.putString("name", names[Random.nextInt(3)])
+        bundle.putLong("date", Random.nextLong(time, time + 1000000))
+
+        val intent = Intent(this, DetailViewActivity::class.java);
+        intent.putExtras(bundle);
+        startActivity(intent)
     }
 }
