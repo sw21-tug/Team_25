46010be8a7bf571d91cb94b5eabd6bd18d1a11d8
Christopher Fartek <fartek@student.tugraz.at>
2021-04-25 18:42:52 +0200

    EXPDAT-008 added more functionality [FC, MF]

--- app/src/main/java/at/sw21_tug/team_25/expirydates/ui/detailview/ui/DetailViewActivity.kt
+++ app/src/main/java/at/sw21_tug/team_25/expirydates/ui/detailview/ui/DetailViewActivity.kt
@@ -1,40 +1,48 @@
 package at.sw21_tug.team_25.expirydates.ui.detailview.ui
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import at.sw21_tug.team_25.expirydates.MainActivity
 import at.sw21_tug.team_25.expirydates.R
 import java.util.*
 
 class DetailViewActivity : AppCompatActivity() {
+    companion object {
+        fun openDetailView(activity: MainActivity, product_id: Int) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+            val inflater: LayoutInflater = activity.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater;
+            var popup_view = inflater.inflate(R.layout.fragment_detail_view, null);
 
-        setContentView(R.layout.fragment_detail_view)
+            var popup_window = PopupWindow(
+                popup_view,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
 
-        val bundle = intent.extras;
+            val name = arrayOf("Milk", "Eggs", "Bread")[product_id];
+            val time = SimpleDateFormat("MM dd", Locale.GERMAN).format(System.currentTimeMillis())
+            popup_view.findViewById<TextView>(R.id.product_name).text = name;
+            popup_view.findViewById<TextView>(R.id.exp_date).text = time;
 
-        if(bundle != null) {
+            popup_window.elevation = 10.0F
 
-            val name = bundle.getString("name").orEmpty();
-            val date_int = bundle.getLong("date");
+            val view = activity.findViewById<View>(android.R.id.content).rootView
 
-            val date = Date(date_int);
+            popup_window.showAtLocation(view, Gravity.CENTER, 0, 0)
 
-            OpenPopup(name, date);
-
+            val closePopUpButton = popup_view.findViewById<Button>(R.id.closePopUp)
+            // set on-click listener
+            closePopUpButton.setOnClickListener {
+                popup_window.dismiss()
+            }
         }
     }
-
-    fun OpenPopup(name : String, date : Date)
-    {
-
-        val product_name = findViewById<TextView>(R.id.product_name);
-        product_name.text = name;
-
-        val exp_date = findViewById<TextView>(R.id.exp_date);
-        exp_date.text = date.toString()
-    }
-
 }
