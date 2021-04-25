06cb1bfd1c8bd4d4e94cc4b5fade19a21fc29a68
Christopher Fartek <fartek@student.tugraz.at>
2021-04-25 18:41:04 +0200

    EXPDAT-008 added basic Detail view, DetailViewActivity.kt [FC, MF]

---
+++ app/src/main/java/at/sw21_tug/team_25/expirydates/ui/detailview/ui/DetailViewActivity.kt
@@ -0,0 +1,35 @@
+package at.sw21_tug.team_25.expirydates.ui.detailview.ui
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import at.sw21_tug.team_25.expirydates.R
+import java.util.*
+
+class DetailViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.fragment_detail_view)
+
+        val bundle = intent.extras;
+
+        if(bundle != null) {
+
+            val name = bundle.getString("name");
+            val date_int = bundle.getLong("date");
+
+            val date = Date(date_int);
+
+
+            val product_name = findViewById<TextView>(R.id.product_name);
+            product_name.text = name;
+
+            val exp_date = findViewById<TextView>(R.id.exp_date);
+            exp_date.text = date.toString()
+
+        }
+    }
+
+}
