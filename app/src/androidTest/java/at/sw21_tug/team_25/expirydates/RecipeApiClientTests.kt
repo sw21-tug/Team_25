fecdb90443565d858982bf4e7cce631c551166eb
Florian <f.marcher@student.tugraz.at>
2021-06-02 14:38:36 +0200

    EXPDAT-007/001 Addedd tests [MF,SS]

--- app/src/androidTest/java/at/sw21_tug/team_25/expirydates/RecipeApiClientTests.kt
+++ app/src/androidTest/java/at/sw21_tug/team_25/expirydates/RecipeApiClientTests.kt
@@ -2,6 +2,7 @@ package at.sw21_tug.team_25.expirydates
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import at.sw21_tug.team_25.expirydates.utils.RecipeAPIClient
+import at.sw21_tug.team_25.expirydates.utils.RecipeInfo
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Assert
@@ -28,14 +29,15 @@ class RecipeApiClientTests {
         val server = MockWebServer()
 
         server.enqueue(
-                MockResponse().setBody(
-                        """
+            MockResponse().setBody(
+                """
                 {
                    "sourceUrl":"SOURCE_URL",
                    "spoonacularSourceUrl":"SPOONACULAR_SOURCE_URL",
                    "id":5
                 }
-                """.trimIndent())
+                """.trimIndent()
+            )
         )
 
         val apiClient = RecipeAPIClient(server.url("").toString())
@@ -45,38 +47,92 @@ class RecipeApiClientTests {
         val request1 = server.takeRequest(5, TimeUnit.SECONDS)
         Assert.assertNotNull(request1)
 
-        Assert.assertEquals("/5/information?apiKey=${RecipeAPIClient.API_KEY}", request1!!.path.toString())
+        Assert.assertEquals(
+            "/5/information?apiKey=${RecipeAPIClient.API_KEY}",
+            request1!!.path.toString()
+        )
         Assert.assertEquals("SPOONACULAR_SOURCE_URL", url)
 
         server.shutdown()
     }
 
-    @Test
-    fun canRequestRecipeInfos() {
+    private val sourceUrl = """ "sourceUrl":"SOURCE_URL", """
+    private val spoonacularSourceUrl = """ "spoonacularSourceUrl":"SPOONACULAR_SOURCE_URL", """
+    private val defaultBody = """{"id": 5,"title": "DemoRecipe","image": "IMAGE_URL"}"""
 
+    @Test
+    fun canRequestRecipeInfosBothURLs() {
+        val infos = testMockServerWithAsserts(sourceUrl, spoonacularSourceUrl)
+        Assert.assertEquals("SPOONACULAR_SOURCE_URL", infos[0].recipeURL)
+    }
+
+    @Test
+    fun canRequestRecipeInfosspooncularURL() {
+        val infos = testMockServerWithAsserts("", spoonacularSourceUrl)
+        Assert.assertEquals("SPOONACULAR_SOURCE_URL", infos[0].recipeURL)
+    }
+
+
+    @Test
+    fun canRequestRecipeInfosSourceURL() {
+        val infos = testMockServerWithAsserts(sourceUrl, "")
+        Assert.assertEquals("SOURCE_URL", infos[0].recipeURL)
+    }
+
+
+    @Test
+    fun canRequestRecipeInfosNoURL() {
+        val infos = testMockServerWithAsserts("", "")
+        Assert.assertEquals("", infos[0].recipeURL)
+    }
+
+    @Test
+    fun canRequestRecipeInfosNoBody() {
+        val infos = testMockServer(sourceUrl, spoonacularSourceUrl, "")
+        Assert.assertEquals(0, infos.size)
+    }
+
+
+    fun testMockServerWithAsserts(
+        sourceUrl: String,
+        spoonacularSourceUrl: String,
+        body: String = defaultBody
+    ): List<RecipeInfo> {
+        val infos = testMockServer(sourceUrl, spoonacularSourceUrl, body)
+
+        Assert.assertEquals(1, infos.size)
+        Assert.assertEquals(5, infos[0].id)
+        Assert.assertEquals("DemoRecipe", infos[0].title)
+        Assert.assertEquals("IMAGE_URL", infos[0].imageURL)
+        return infos
+    }
+
+    fun testMockServer(
+        sourceUrl: String,
+        spoonacularSourceUrl: String,
+        body: String
+    ): List<RecipeInfo> {
         val server = MockWebServer()
 
         server.enqueue(
-                MockResponse().setBody(
-                        """
+            MockResponse().setBody(
+                """
                 [
-                    {
-                        "id": 5,
-                        "title": "DemoRecipe",
-                        "image": "IMAGE_URL"
-                    }
+                    $body
                 ]
-                """.trimIndent())
+                """.trimIndent()
+            )
         )
         server.enqueue(
-                MockResponse().setBody(
-                        """
+            MockResponse().setBody(
+                """
                 {
-                   "sourceUrl":"SOURCE_URL",
-                   "spoonacularSourceUrl":"SPOONACULAR_SOURCE_URL",
+                    $sourceUrl
+                    $spoonacularSourceUrl
                    "id":5
                 }
-                """.trimIndent())
+                """.trimIndent()
+            )
         )
 
         val apiClient = RecipeAPIClient(server.url("").toString())
@@ -84,16 +140,13 @@ class RecipeApiClientTests {
 
         val infos = apiClient.getRecipeForIngredient("tomato", 1)
         val request1 = server.takeRequest(5, TimeUnit.SECONDS)
-        Assert.assertNotNull(request1)
-
-        Assert.assertEquals("/findByIngredients?ingredients=tomato&number=1&ignorePantry=true&ranking=1&apiKey=${RecipeAPIClient.API_KEY}", request1!!.path.toString())
-
-        Assert.assertEquals(1, infos.size)
-        Assert.assertEquals(5, infos[0].id)
-        Assert.assertEquals("DemoRecipe", infos[0].title)
-        Assert.assertEquals("IMAGE_URL", infos[0].imageURL)
-        Assert.assertEquals("SPOONACULAR_SOURCE_URL", infos[0].recipeURL)
-
         server.shutdown()
+
+        Assert.assertNotNull(request1)
+        Assert.assertEquals(
+            "/findByIngredients?ingredients=tomato&number=1&ignorePantry=true&ranking=1&apiKey=${RecipeAPIClient.API_KEY}",
+            request1!!.path.toString()
+        )
+        return infos
     }
 }
