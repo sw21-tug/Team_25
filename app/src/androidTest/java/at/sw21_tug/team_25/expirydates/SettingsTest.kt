c2df132fd7d438cb8fff8aad21552dd17d52f08b
Esther Isimekhai <isimekhai@student.tugraz.at>
2021-06-02 09:37:29 +0200

    EXPDAT-015/001 Initial commit with test class for Settings [FC, IE]

---
+++ app/src/androidTest/java/at/sw21_tug/team_25/expirydates/SettingsTest.kt
@@ -0,0 +1,13 @@
+package at.sw21_tug.team_25.expirydates
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.*
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+@FixMethodOrder
+class SettingsTest {
+    }
+
